c748cb0b8a554d4597f21b1ce6db5ea31be9f171
Shuster <henry.shuster19@mbpr1315-1785.trinity.nyc.ny.us>
2018-12-14 13:02:22 -0500

    updated

--- data_server.js
+++ data_server.js
@@ -9,9 +9,9 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(favicon(__dirname + '/public/images/logo.png'));
 
-var port = 3000;
+var port = process.env.PORT || 3000; //||8000
 app.listen(port, function(){
-  console.log('Server started at '+ new Date()+', on port ' + port+'!');
+  console.log('Easy server listening for requests on port'+ port+'!');
 });
 
 app.get('/', function(request, response){
@@ -47,22 +47,28 @@ app.get('/rules', function(request, response){
 
 app.get('/stats', function(request, response){
   var user_data=[];
-  var user_file=fs.readFileSync("data/users.csv","utf8");
-  var user_lines =user_file.split('\n');
+//load the data fromt the csv
+  var user_file=fs.readFileSync("data/users.csv", "utf8");
   console.log(user_file);
-  console.log(user_lines);
-  for(var i=1; i<user_lines.length; i++){
-    var single_user=user_lines[i].trim().split(',');
-    var user_object={}
 
+//parse the data from the csv into a non-string format
+  var user_lines = user_file.split('\n');
+  console.log(user_lines);
+  for(var i=1; i<user_lines.length-1; i++){
+    var user_object={};
+    var single_user = user_lines[i].trim().split(",");
     user_object["name"]=single_user[0];
     user_object["games played"]=parseInt(single_user[1]);
     user_object["games won"]=parseInt(single_user[2]);
     user_object["games lost"]=parseInt(single_user[3]);
+    user_object["paper"]=parseInt(single_user[4]);
+    user_object["rock"]=parseInt(single_user[5]);
+    user_object["scissors"]=parseInt(single_user[6]);
 
     user_data.push(user_object);
   }
   console.log(user_data);
+
   response.status(200);
   response.setHeader('Content-Type', 'text/html')
   response.render('stats',{users:user_data});
