4fe3916ba6ab5c6efaad4c5e777a2ce454732fd3
Lila Selin/ <lila.selin19@trinityschoolnyc.org>
2019-01-16 15:34:54 -0500

    Image updates on game page

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,11 +1,15 @@
 
 ///////////////////Event Listeners//////////////////
+var weapons=document.getElementById("weapon_select");
+var p_img=document.getElementById("player_hand");
+weapons.addEventListener("change",function(){
+  if(weapons.value=="")p_img.src="../images/ _waiting.svg";
+  else p_img.src="../images/player_"+weapons.value+".png";
+});
+
 var villains=document.getElementById("villain_select");
-var img = document.getElementById("villain_hand");
+var v_img = document.getElementById("villain_hand");
 villains.addEventListener("change",function(){
-  console.log(villains.value);
-  console.log(img.src);
-  img.src="../images/"+villains.value+"_waiting.svg";
-  console.log(img.src);
+  v_img.src="../images/"+villains.value+"_waiting.svg";
 });
 ///////////////////Helper function//////////////////
