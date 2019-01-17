3a4971e9ab331d9b6ecabbfc8d6b8262d748d6a2
Shuster <henry.shuster19@mbpr1315-1785.trinity.nyc.ny.us>
2019-01-17 10:54:27 -0500

    referee

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -6,6 +6,15 @@ weapons.addEventListener("change",function(){
   if(weapons.value=="")p_img.src="../images/ _waiting.svg";
   else p_img.src="../images/player_"+weapons.value+".png";
 });
+setTimeout(function(){document.getElementById("ref").src="../images/ref/two.svg";
+document.getElementById("ref_text").innerHTML="Two!"}, 1000);
+setTimeout(function(){document.getElementById("ref").src="../images/ref/one.svg";
+document.getElementById("ref_text").innerHTML="One!"}, 2000);
+setTimeout(function(){document.getElementById("ref").src="../images/ref/fist_bump.svg"
+document.getElementById("ref_text").innerHTML="Shoot!"}, 3000);
+setTimeout(function(){document.getElementById("ref").src="../images/ref/watch.svg";
+document.getElementById("ref_text").innerHTML="You're taking too long!!!"}, 10000);
+
 
 var villains=document.getElementById("villain_select");
 var v_img = document.getElementById("villain_hand");
